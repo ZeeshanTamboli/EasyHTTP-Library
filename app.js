e336336a900edafb8f86cf221968bc163a860852
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-07-12 22:35:50 +0530

    Delete post

--- app.js
+++ app.js
@@ -1,9 +1,52 @@
 const http = new easyHTTP();
 
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//GET request
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+//Create data
+const data = {
+  title: 'New Post',
+  body: 'This is a new post.'
+};
+
+//Create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+//Update post
+// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(
+//   err,
+//   post
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(
+  err,
+  response
+) {
   if (err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(response);
   }
 });
